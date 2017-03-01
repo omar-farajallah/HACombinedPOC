4db6bad7e10c75fa738473bfba729e1e15531247
Omar Farajallah <omar.farajallah@slalom.com>
2017-03-01 14:33:30 -0800

    React Baseline for refinement

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        // Our main app
+        app: ['../ha/ui.apps/src/main/content/jcr_root/apps/hollandamericatraining/components/react/App.js'],
+
+        // React Habitat requires Object.assign pollyfill for old IE support
+        vendor: ['babel-polyfill', 'react', 'react-dom', 'react-habitat']
+    },
+    output: {
+        filename: '../ha/ui.apps/src/main/content/jcr_root/etc/designs/hollandamericatraining/js/app-bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.jsx?$/, // Matches .js and .jsx files
+                exclude:/node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+        ]
+    },
+    plugins: [
+
+        // Split our app code and libraries. See "Entry" config above
+        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"../ha/ui.apps/src/main/content/jcr_root/etc/designs/hollandamericatraining/js/vendor-bundle.js"),
+    ]
+};
